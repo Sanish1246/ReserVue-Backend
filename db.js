04afb7ea034645d5d1bc9f0420a14797b726a05f
Sanish1246 <Sanishferrari@gmail.com>
2025-10-14 10:07:05 +0400

    Adding Database connection

---
+++ db.js
@@ -0,0 +1,29 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+//uri connection to the database
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//Function to connect to the database
+export async function connectToDb() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to the E-commerce databse on MongoDB!"
+    );
+    return client;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+connectToDb().catch(console.dir);
