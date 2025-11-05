aa466f811792ea83564ca4a0e898466fee860ee6
Sanish1246 <Sanishferrari@gmail.com>
2025-11-05 14:51:58 +0400

    update logger function

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ import ordersRoutes from "./routes/orders.js";
 function logger(req, res, next) {
   const now = new Date();
   console.log(`[${now.toISOString()}] ${req.method} ${req.originalUrl}`);
-  console.log("Headers:", req.headers);
-  console.log("Body:", req.body);
   console.log("--------------------------------------------------");
   next();
 }
